809862c1e763b430eec984a44b0cfcf5c792ca7d
gabrielayresdev <gabriel.ar.fort@gmail.com>
2026-02-18 06:05:45 -0300

    feat: meal migration

---
+++ db/migrations/20260218090108_create-meal.ts
@@ -0,0 +1,13 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("meals", (table) => {
+    table.uuid("id").primary();
+    table.string("name").notNullable();
+    table.string("description").notNullable();
+    table.timestamp("date_time").notNullable();
+    table.boolean("is_on_diet").notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {}
