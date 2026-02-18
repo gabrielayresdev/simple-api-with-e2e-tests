4555e14e1b32bb9d98ed6e488884b21e3b36f669
gabrielayresdev <gabriel.ar.fort@gmail.com>
2026-02-18 20:45:25 -0300

    feat: add @fastify/cors dependency and register CORS middleware in app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import fastify from "fastify";
 import { mealRoutes } from "./routes/routes";
 import cookie from "@fastify/cookie";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify();
 
 app.register(cookie);
+
+app.register(fastifyCors, { origin: "*" });
 
 app.register(mealRoutes);
 
